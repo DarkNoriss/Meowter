fab4b70867b66bb04a167760a7ec675f9764d9dc
Patryk Przybylski <darknoriss@gmail.com>
2023-06-30 21:52:36 +0200

    create feed component

---
+++ components/Feed.tsx
@@ -0,0 +1,13 @@
+import { MeowCard } from './MeowCard';
+
+export const Feed = () => {
+  return (
+    <div>
+      <MeowCard />
+      <MeowCard />
+      <MeowCard />
+      <MeowCard />
+      <MeowCard />
+    </div>
+  );
+};
