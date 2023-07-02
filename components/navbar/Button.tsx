98f6854df7046356761c8cb685712bcf255754bd
Patryk Przybylski <darknoriss@gmail.com>
2023-07-03 01:51:23 +0200

    add bold navigation if on this page

--- components/navbar/Button.tsx
+++ components/navbar/Button.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export const Button = ({ name }: { name: string }) => {
   const [isHere, setIsHere] = useState<boolean>(false);
+  const path = usePathname();
+
+  useEffect(() => {
+    const isPathMatched = (name === 'home' && path === '/') || path === `/${name}`;
+    setIsHere(isPathMatched);
+  }, [name, path]);
 
   return (
     <div className='btnhover my-1 flex h-14 flex-row p-3'>
