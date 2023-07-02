a17cc6cb32601f7be54dc0f3b4a3182d4732f76d
Patryk Przybylski <darknoriss@gmail.com>
2023-07-03 01:50:18 +0200

    add if session doesnt exist display login popout

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,20 +1,22 @@
 'use client';
 
-import { useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 
 export const Login = () => {
-  const [isLogged, setIsLogged] = useState<Boolean>(false);
+  const { data: session } = useSession();
 
   return (
     <>
-      {!isLogged && (
+      {!session && (
         <div className='fixed bottom-0 left-0 flex w-full flex-row justify-around bg-gray-500'>
           <div className='flex flex-col p-4'>
             <span className='text-2xl font-bold'>{`Don't miss that's happening`}</span>
             <span>{`People on Meowter are the first to know`}</span>
           </div>
           <div className='flex-center'>
-            <span className='btnhover border px-4 py-2 text-xl font-bold'>Log in</span>
+            <button className='btnhover border px-4 py-2' onClick={() => signIn()}>
+              <span className='text-xl font-bold'>Log in</span>
+            </button>
           </div>
         </div>
       )}
