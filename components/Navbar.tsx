526326ee5e20917333b5994aefb1bb466e8e4941
Patryk Przybylski <darknoriss@gmail.com>
2023-07-03 22:49:19 +0200

    add display things acording to if user is logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,32 @@ import Image from 'next/image';
 import { Button } from './navbar/Button';
 import { useSession } from 'next-auth/react';
 import { strToLink } from '@/utils/strToLink';
+import { RefObject, useEffect, useRef, useState } from 'react';
+import { signOut } from 'next-auth/react';
 
 export const Navbar = () => {
   const { data: session } = useSession();
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const menuRef: RefObject<HTMLDivElement> = useRef(null);
+
+  useEffect(() => {
+    const handler = (event: MouseEvent) => {
+      if (!menuRef.current?.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handler);
+
+    return () => {
+      document.body.removeEventListener('click', handler);
+    };
+  }, []);
+
+  const handleOpen = () => {
+    if (!open) setOpen(true);
+  };
 
   return (
     <header className='flex h-screen w-72 flex-col justify-between pl-2 pr-8'>
@@ -29,8 +52,8 @@ export const Navbar = () => {
         </div>
       </div>
       {session && (
-        <div className='btnhover my-3 flex items-center p-3'>
-          <div>
+        <div className='relative' onClick={handleOpen} ref={menuRef}>
+          <div className={`${open ? '' : 'btnhover'} my-3 flex w-full items-center p-3`}>
             <Image
               src={session.user?.image as string}
               alt='Avatar'
@@ -38,20 +61,29 @@ export const Navbar = () => {
               width={40}
               className='rounded-full'
             />
+            <div className='mx-3 flex flex-col text-base'>
+              <span className='font-bold'>{session.user?.name}</span>
+              <span className='font-normal text-gray-500'>
+                {strToLink(session.user?.name as string)}
+              </span>
+            </div>
+            <div className='flex flex-1 justify-end'>
+              <svg viewBox='0 0 24 24' aria-hidden='true' className='h-5 fill-white '>
+                <g>
+                  <path d='M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z'></path>
+                </g>
+              </svg>
+            </div>
           </div>
-          <div className='mx-3 flex flex-col text-sm'>
-            <span className='font-bold'>{session.user?.name}</span>
-            <span className='font-normal text-gray-500'>
-              {strToLink(session.user?.name as string)}
-            </span>
-          </div>
-          <div className='flex flex-1 justify-end'>
-            <svg viewBox='0 0 24 24' aria-hidden='true' className='h-5 fill-white '>
-              <g>
-                <path d='M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z'></path>
-              </g>
-            </svg>
-          </div>
+          {open && (
+            <div className='shadow-glow shadow-white border-white-smoll active absolute bottom-1/2 left-1/2 w-72 -translate-x-1/2 -translate-y-1/2 rounded-xl py-3'>
+              <div className='btnhover !rounded-none px-4 py-3' onClick={() => signOut()}>
+                <span className='text-base font-bold'>{`Log out ${strToLink(
+                  session.user?.name as string
+                )}`}</span>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </header>
