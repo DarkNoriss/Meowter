efb06104c31cd7030b52b3134b41c403f6f0dad8
Patryk Przybylski <darknoriss@gmail.com>
2023-07-04 05:46:40 +0200

    add usestate for future posts

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useState } from 'react';
 import { MeowCard } from './MeowCard';
 
 export const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className='max-h-full '>
       <MeowCard />
