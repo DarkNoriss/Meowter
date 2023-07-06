c6b78e26a7c706f371977989aa4afe21b96ee155
Patryk Przybylski <darknoriss@gmail.com>
2023-07-07 00:11:02 +0200

    add: image from next

--- components/navbar/Button.tsx
+++ components/navbar/Button.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
@@ -13,15 +14,7 @@ export const Button = ({ name }: { name: string }) => {
   return (
     <div className='btnhover my-1 flex h-14 flex-row p-3'>
       <div>
-        <svg
-          viewBox='0 0 24 24'
-          aria-hidden='true'
-          className='aspect-square h-7 fill-white'
-        >
-          <g>
-            <path d='M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z'></path>
-          </g>
-        </svg>
+        <Image src={`/assets/icons/${name}.svg`} alt='logo' height={28} width={28} />
       </div>
       <span className={`ml-5 text-xl ${isHere ? 'font-bold' : ''} capitalize`}>
         {name}
