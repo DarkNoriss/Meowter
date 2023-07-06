2369c278b9e973439cc611b47551992cf67f856b
Patryk Przybylski <darknoriss@gmail.com>
2023-07-06 05:27:20 +0200

    add: fetch meows

--- components/MeowList.tsx
+++ components/MeowList.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react';
+
+type MeowType = {
+  context: string;
+  date: Date;
+  ta: {
+    remeows: number;
+    comments: number;
+    likes: number;
+  };
+  _id: string;
+};
 export const MeowList = () => {
+  const [meows, setMeows] = useState<MeowType[]>([]);
+
+  useEffect(() => {
+    fetchMeows();
+  }, []);
+
+  const fetchMeows = async () => {
+    const response = await fetch('api/meow');
+    const data = await response.json();
+
+    setMeows(data);
+  };
+
+  console.log(meows[0]);
   return <div>MeowList</div>;
 };
