3321ac0ebb1c09157c93e738aa17584a6d5e64a3
Patryk Przybylski <darknoriss@gmail.com>
2023-07-06 04:37:41 +0200

    cleaning

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,13 +1,8 @@
 'use client';
 
-import { useState } from 'react';
 import { MeowCard } from './MeowCard';
-import { connectToDB } from '@/utils/connectToDB';
 
 export const Feed = () => {
-  const hey = connectToDB();
-  const [posts, setPosts] = useState([]);
-
   return (
     <div className=''>
       <MeowCard />
