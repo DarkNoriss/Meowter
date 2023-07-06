aa4f9bff4ace2f602230dbbd0ec576d94ba28c82
Patryk Przybylski <darknoriss@gmail.com>
2023-07-06 04:36:18 +0200

    add: id to user session and save use to db

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,8 @@
-import type { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/Google';
+
+import User from '@/models/user';
+import { connectToDB } from '@/utils/connectToDB';
 
 export const options: NextAuthOptions = {
   providers: [
@@ -8,4 +11,34 @@ export const options: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session?.user?.email });
+
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        const userExists = await User.findOne({ email: profile?.email });
+
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name,
+            image: profile?.image,
+          });
+        }
+
+        return true;
+      } catch (err: any) {
+        console.log('Error checking if user exists: ', err.message);
+        return false;
+      }
+    },
+  },
 };
