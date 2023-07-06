3ff861708ecbe3fb80ccc73cd7ae2f301be8a50d
Patryk Przybylski <darknoriss@gmail.com>
2023-07-06 19:25:46 +0200

    remove: temp card no longer needed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,52 +1,11 @@
 'use client';
 
-import { MeowCardTemp } from './MeowCardTemp';
 import { MeowList } from './MeowList';
 
 export const Feed = () => {
   return (
-    <div className=''>
+    <>
       <MeowList />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-      <MeowCardTemp />
-    </div>
+    </>
   );
 };
