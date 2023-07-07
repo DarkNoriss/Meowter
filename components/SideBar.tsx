bcb7d8238a8ae41d3c31e21cde01defb806cd1c1
Patryk Przybylski <darknoriss@gmail.com>
2023-07-07 20:49:44 +0200

    add: sidebar with search bar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,19 +3,24 @@ import Image from 'next/image';
 export const SideBar = () => {
   return (
     <div>
-      <div className='border-white-smoll flex h-screen flex-col justify-between !border-y-0 !border-r-0 pl-7 pr-7'>
-        <div className='fixed top-0 mt-2 bg-slate-400 '>
-          <div className='mb-3 flex w-80'>
-            <div>
-              <Image
-                src={`/assets/icons/explore.svg`}
-                alt='logo'
-                height={20}
-                width={20}
-              />
-            </div>
-            <div className='p-3'>
-              <input placeholder='Search Meowter' />
+      <div className='h-screen w-96'>
+        <div className='border-white-smoll fixed top-0 h-full w-96 !border-y-0 !border-r-0 pl-7'>
+          <div className='mt-2'>
+            <div className='flex-center w-full rounded-full bg-slate-800'>
+              <div className='pl-4'>
+                <Image
+                  src={`/assets/icons/explore.svg`}
+                  alt='logo'
+                  height={20}
+                  width={20}
+                />
+              </div>
+              <div className='flex-1 p-3'>
+                <input
+                  className='w-full bg-transparent text-xl'
+                  placeholder='Search Meowter'
+                />
+              </div>
             </div>
           </div>
         </div>
