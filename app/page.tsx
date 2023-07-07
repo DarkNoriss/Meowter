7330c8506672d247a940d748502c559bee207fe8
Patryk Przybylski <darknoriss@gmail.com>
2023-07-07 20:49:15 +0200

    move: home comp to page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import { Home as HomeComp } from '@/components/Home';
+'use client';
+
+import { Feed } from '@/components/Feed';
+import { Form } from '@/components/Form';
+import { useSession } from 'next-auth/react';
 
 const Home = () => {
-  return <HomeComp />;
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div className='border-white-smoll bg-transblur sticky top-0 z-10 flex h-14 w-full max-w-xl items-center !border-t-0 px-4 backdrop-blur-md backdrop-filter'>
+        <span className='text-xl font-bold'>Home</span>
+      </div>
+      <div className='-z-10'>
+        {session && <Form />}
+        <Feed />
+      </div>
+    </>
+  );
 };
 
 export default Home;
