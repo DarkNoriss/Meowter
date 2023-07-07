6309ce3487b7e1d80a1c40411b7213627b15e1f3
Patryk Przybylski <darknoriss@gmail.com>
2023-07-07 21:53:31 +0200

    cleaning

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
   return (
     <header>
       <div className='h-screen w-72'>
-        <div className='border-white-smoll fixed top-0 h-full w-72 !border-y-0 !border-l-0  pr-7'>
+        <div className='border-white-smoll fixed top-0 h-full w-72 !border-y-0 !border-l-0 pr-7'>
           <div className='flex h-full flex-col justify-between'>
             <div className=''>
               <Link
