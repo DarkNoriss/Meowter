0a01992dff6a4b8701b21b0cc64bf109bf4485f6
Patryk Przybylski <darknoriss@gmail.com>
2023-07-08 05:47:29 +0200

    add: fetch user, users meow and store them

--- context/meowContext.tsx
+++ context/meowContext.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { MeowType } from '@/types/custom-types';
+import { MeowType, UserType } from '@/types/custom-types';
 import { ReactNode, createContext, useContext, useReducer, useState } from 'react';
 
 type MeowterContextType = {
   fetchMeows: () => Promise<void>;
+  fetchUser: ({ userLink }: { userLink: string }) => Promise<void>;
+  fetchUserMeows: ({ userId }: { userId: string }) => Promise<void>;
   getMeows: () => MeowType[];
+  getUser: () => UserType[];
+  getUsersMeows: () => MeowType[];
 };
 
 const MeowterContext = createContext({} as MeowterContextType);
@@ -16,6 +20,8 @@ export const useMeowterContext = () => {
 
 export const MeowterProvider = ({ children }: { children: ReactNode }) => {
   const [meows, setMeows] = useState<MeowType[]>([]);
+  const [user, setUser] = useState<UserType[]>([]);
+  const [userMeows, setUserMeows] = useState<MeowType[]>([]);
 
   const fetchMeows = async () => {
     const response = await fetch('api/meow');
@@ -24,10 +30,28 @@ export const MeowterProvider = ({ children }: { children: ReactNode }) => {
     setMeows(data);
   };
 
+  const fetchUser = async ({ userLink }: { userLink: string }) => {
+    const response = await fetch(`api/users/${userLink}`);
+    const data = await response.json();
+
+    setUser(data);
+  };
+
+  const fetchUserMeows = async ({ userId }: { userId: string }) => {
+    const response = await fetch(`api/users/${userId}/meow`);
+    const data = await response.json();
+
+    setUserMeows(data);
+  };
+
   const getMeows = () => meows;
+  const getUser = () => user;
+  const getUsersMeows = () => userMeows;
 
   return (
-    <MeowterContext.Provider value={{ fetchMeows, getMeows }}>
+    <MeowterContext.Provider
+      value={{ fetchMeows, fetchUser, fetchUserMeows, getMeows, getUser, getUsersMeows }}
+    >
       {children}
     </MeowterContext.Provider>
   );
