7778d9b3d36b5a7fb492012b4a290ff28e6e4798
Patryk Przybylski <darknoriss@gmail.com>
2023-07-09 14:35:16 +0200

    move: fetch in comp, styling,

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,29 +1,37 @@
 'use client';
 
 import { FeedProfile } from '@/components/FeedProfile';
-import { useMeowterContext } from '@/context/meowContext';
+import { UserType } from '@/types/custom-types';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Profile = ({ params }: { params: { id: string } }) => {
-  const { fetchUser, getUser } = useMeowterContext();
+  const [userData, setUserData] = useState<UserType[]>([]);
+  const [userMeowsNb, setUserMeowsNb] = useState<number>(0);
 
   useEffect(() => {
-    fetchUser(params);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const fetchUser = async () => {
+      console.log('Fetching user...');
+      const response = await fetch(`/api/users/${params.id}`);
+      const data = await response.json();
+
+      setUserData(data);
+    };
+
+    fetchUser();
+  }, [params]);
 
   return (
     <>
-      {getUser().length > 0 && getUser()[0].userlink === params.id && (
-        <>
+      {userData.map((user) => (
+        <div key={user._id}>
           <div className='border-white-smoll bg-transblur sticky top-0 flex h-14 w-full max-w-xl items-center !border-t-0 px-4 backdrop-blur-md backdrop-filter'>
             <div className='min-w-[56px]'>
               <Image src={`/assets/icons/arrow.svg`} alt='logo' height={22} width={22} />
             </div>
             <div className='flex flex-col'>
-              <span className='text-xl font-bold'>{getUser()?.[0].username}</span>
-              <span className='text-sm text-gray-500'>987 Meows</span>
+              <span className='text-xl font-bold'>{user.username}</span>
+              <span className='text-sm text-gray-500'>{userMeowsNb} Meows</span>
             </div>
           </div>
           <div>
@@ -31,28 +39,30 @@ const Profile = ({ params }: { params: { id: string } }) => {
               <Image src={`/assets/img/bg.jpg`} alt='logo' height={500} width={1500} />
             </div>
             <div className='mb-4 px-4 pt-3'>
-              <div className='flex '>
-                <div>
-                  <Image
-                    src={getUser()?.[0].image}
-                    alt='Avatar'
-                    height={40}
-                    width={40}
-                    className='rounded-full'
-                  />
+              <div className='-mb-4 flex justify-between'>
+                <div className='mt-[15%] w-1/4'>
+                  <div className='relative bottom-0 left-0 top-0'>
+                    <div className='absolute bottom-1/2 -translate-y-1/4'>
+                      <Image
+                        src={user.image}
+                        alt='Avatar'
+                        height={135}
+                        width={135}
+                        className='rounded-full border-4 border-gray-900'
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <span>Edit profile</span>
+                <div className='flex-center btnhover mb-3 h-9 rounded-full border px-4'>
+                  <span className='text-base font-bold'>Edit profile</span>
                 </div>
               </div>
               <div className='mb-3 mt-1 flex flex-col'>
                 <div>
-                  <span className='text-xl font-bold'>{getUser()?.[0].username}</span>
+                  <span className='text-xl font-bold'>{user.username}</span>
                 </div>
                 <div>
-                  <span className='font-normal text-gray-500'>
-                    @{getUser()?.[0].userlink}
-                  </span>
+                  <span className='font-normal text-gray-500'>@{user.userlink}</span>
                 </div>
               </div>
               <div className='flex flex-row font-normal text-gray-500'>
@@ -88,9 +98,9 @@ const Profile = ({ params }: { params: { id: string } }) => {
               </div>
             </div>
           </div>
-          <FeedProfile />
-        </>
-      )}
+          <FeedProfile id={user._id} setUserMeowsNb={setUserMeowsNb} />
+        </div>
+      ))}
     </>
   );
 };
