446ab2ef91d7289a3fd56254675f770e66dca4cb
Patryk Przybylski <darknoriss@gmail.com>
2023-07-11 23:01:11 +0200

    use: swr to fetch

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,20 +1,25 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { MeowCard } from './MeowCard/MeowCard';
 import { useMeowterContext } from '@/context/meowContext';
+import useSWR from 'swr';
+import { MeowType } from '@/types/custom-types';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const Feed = () => {
-  const { fetchMeows, meows } = useMeowterContext();
+  // const { fetchMeows, meows } = useMeowterContext();
+  const { data, isLoading } = useSWR<MeowType[]>('/api/meow', fetcher);
 
-  useEffect(() => {
-    fetchMeows();
-  }, [fetchMeows]);
-
+  // useEffect(() => {
+  //   //fetchMeows();
+  // }, [fetchMeows]);
+  if (isLoading) return 'Loading...';
   return (
     <>
-      {meows.map((meow) => (
+      {data?.map((meow) => (
         <MeowCard key={meow._id} meow={meow} />
       ))}
     </>
