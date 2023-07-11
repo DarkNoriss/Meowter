15669e3286293c055b0e6c52809852084b795733
Patryk Przybylski <darknoriss@gmail.com>
2023-07-11 06:25:25 +0200

    add: save new meow id to user meows

--- app/api/meow/new/route.ts
+++ app/api/meow/new/route.ts
@@ -2,23 +2,34 @@ import Meow from '@/models/meow';
 import { connectToDB } from '@/utils/connectToDB';
 import { getServerSession } from 'next-auth';
 import { options } from '../../auth/[...nextauth]/options';
+import User from '@/models/user';
+import { updateUsersWithMeowsField } from '@/utils/updateUsersWithMeowsField';
 
 export const POST = async (req: Request) => {
   const { context } = await req.json();
   const session = await getServerSession(options);
 
   try {
-    await connectToDB();
+    if (session) {
+      await connectToDB();
 
-    const newMeow = new Meow({
-      creator: session?.user.id,
-      context,
-      date: new Date(),
-    });
+      const newMeow = new Meow({
+        creator: session.user.id,
+        context,
+        date: new Date(),
+      });
 
-    await newMeow.save();
+      await newMeow.save();
 
-    return new Response(JSON.stringify(newMeow), { status: 201 });
+      const user = await User.findById(session.user.id);
+
+      if (user) {
+        user.meows.push(newMeow._id);
+        await user.save();
+      }
+
+      return new Response(JSON.stringify(newMeow), { status: 201 });
+    }
   } catch (err) {
     return new Response('Failed to create a new meow', { status: 500 });
   }
