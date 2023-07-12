f2b622f67b703a7b36382445009939ab5e41ebec
Patryk Przybylski <darknoriss@gmail.com>
2023-07-12 17:46:02 +0200

    change: send meow using prisma

--- app/api/meow/new/route.ts
+++ app/api/meow/new/route.ts
@@ -1,38 +1,25 @@
-import Meow from '@/models/meow';
 import { getServerSession } from 'next-auth';
 import { options } from '../../auth/[...nextauth]/options';
-import User from '@/models/user';
-import { updateMeowReactions } from '@/utils/updateMeowReactions';
-import { createUsers } from '@/utils/createUsers';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const POST = async (req: Request) => {
   const { context } = await req.json();
   const session = await getServerSession(options);
 
-  createUsers();
-  // updateMeowReactions();
-  // try {
-  //   if (session) {
-  //     await connectToDB();
+  try {
+    if (session) {
+      const newMeow = await prisma.meow.create({
+        data: {
+          authorId: session.user.id as string,
+          text: context,
+        },
+      });
 
-  //     const newMeow = new Meow({
-  //       creator: session.user.id,
-  //       context,
-  //       date: new Date(),
-  //     });
-
-  //     await newMeow.save();
-
-  //     const user = await User.findById(session.user.id);
-
-  //     if (user) {
-  //       user.meows.push(newMeow._id);
-  //       await user.save();
-  //     }
-
-  //     return new Response(JSON.stringify(newMeow), { status: 201 });
-  //   }
-  // } catch (err) {
-  //   return new Response('Failed to create a new meow', { status: 500 });
-  // }
+      return new Response(JSON.stringify(newMeow), { status: 201 });
+    }
+  } catch (err) {
+    return new Response('Failed to create a new meow', { status: 500 });
+  }
 };
