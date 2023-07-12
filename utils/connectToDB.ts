2ba513b4c878c4950fadc0926be7e97838d44675
Patryk Przybylski <darknoriss@gmail.com>
2023-07-12 10:45:53 +0200

    fix: remove guard

--- utils/connectToDB.ts
+++ utils/connectToDB.ts
@@ -5,8 +5,6 @@ let isConnected = false;
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if (isConnected) return console.log('MongoDB is already connected');
-
   try {
     await mongoose.connect(
       process.env.MONGODB_URI as string,
