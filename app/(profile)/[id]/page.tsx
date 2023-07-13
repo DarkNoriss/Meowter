43f99773a2dcf7ee2af6166b8e3e4e6cd59c831f
Patryk Przybylski <darknoriss@gmail.com>
2023-07-13 12:07:46 +0200

    add: moving to component, styling

--- app/(profile)/[id]/page.tsx
+++ app/(profile)/[id]/page.tsx
@@ -6,6 +6,7 @@ import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import { UserWithMeows } from '@/types/custom-types';
 import { Feed } from '@/components/Feed';
+import { ProfileNavigation } from '@/components/Profile/ProfileNavigation';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -17,10 +18,17 @@ const Profile = ({ params }: { params: { id: string } }) => {
     <>
       {data && (
         <div key={data.id}>
-          <div className='border-white-smoll bg-transblur sticky top-0 flex h-14 w-full max-w-xl items-center !border-t-0 px-4 backdrop-blur-md backdrop-filter'>
-            <Link href='/' className='min-w-[56px]' passHref>
-              <Image src={`/assets/icons/arrow.svg`} alt='logo' height={22} width={22} />
-            </Link>
+          <div className='border-white-smoll bg-transblur sticky top-0 z-10 flex h-14 w-full max-w-xl items-center !border-t-0 px-4 backdrop-blur-md backdrop-filter'>
+            <div className='btnhover mr-4 p-2'>
+              <Link href='/' className='min-w-[56px]' passHref>
+                <Image
+                  src={`/assets/icons/arrow.svg`}
+                  alt='logo'
+                  height={22}
+                  width={22}
+                />
+              </Link>
+            </div>
             <div className='flex flex-col'>
               <span className='text-xl font-bold'>{data.username}</span>
               <span className='text-sm text-gray-500'>{data.meows.length} Meows</span>
@@ -31,7 +39,7 @@ const Profile = ({ params }: { params: { id: string } }) => {
               <Image src={`/assets/img/bg.jpg`} alt='logo' height={500} width={1500} />
             </div>
             <div className='mb-4 px-4 pt-3'>
-              <div className='-mb-4 flex justify-between'>
+              <div className='-mb-4 flex justify-between lg:-mb-6'>
                 <div className='mt-[15%] w-1/4'>
                   <div className='relative'>
                     <div className='absolute bottom-1/2 -translate-y-1/4'>
@@ -51,47 +59,21 @@ const Profile = ({ params }: { params: { id: string } }) => {
                   </div>
                 )}
               </div>
-              <div className='mb-3 mt-1 flex flex-col'>
-                <div>
-                  <span className='text-xl font-bold'>{data.username}</span>
-                </div>
-                <div>
-                  <span className='font-normal text-gray-500'>@{data.userlink}</span>
-                </div>
+              <div className=' mb-3 flex flex-col'>
+                <span className='text-xl font-bold'>{data.username}</span>
+                <span className='text-sm font-normal text-gray-500 lg:text-base'>
+                  @{data.userlink}
+                </span>
               </div>
               <div className='flex flex-row font-normal text-gray-500'>
-                {data.birtday_date && <span className='mr-5'>Born</span>}
-                <span>Joined</span>
+                {data.birtday_date && (
+                  <span className='mr-5 text-sm lg:text-base'>Born</span>
+                )}
+                <span className='text-sm lg:text-base'>Joined</span>
               </div>
             </div>
           </div>
-          <div className='border-white-smoll flex h-14 flex-row !border-t-0 font-normal text-gray-500'>
-            <div className='flex-center btnhover flex-1 !rounded-none px-4'>
-              <div className='flex-center h-full'>
-                <span>Meows</span>
-              </div>
-            </div>
-            <div className='flex-center btnhover flex-1 !rounded-none px-4'>
-              <div className='flex-center h-full'>
-                <span>Replies</span>
-              </div>
-            </div>
-            <div className='flex-center btnhover flex-1 !rounded-none px-4'>
-              <div className='flex-center h-full'>
-                <span>Highlights</span>
-              </div>
-            </div>
-            <div className='flex-center btnhover flex-1 !rounded-none px-4'>
-              <div className='flex-center h-full '>
-                <span>Media</span>
-              </div>
-            </div>
-            <div className='flex-center btnhover flex-1 !rounded-none px-4'>
-              <div className='flex-center h-full'>
-                <span>Likes</span>
-              </div>
-            </div>
-          </div>
+          <ProfileNavigation />
           <Feed meows={data.meows} />
         </div>
       )}
---
+++ components/Profile/ProfileNavigation.tsx
@@ -0,0 +1,21 @@
+export const ProfileNavigation = () => {
+  return (
+    <div className='border-white-smoll flex h-14 flex-row !border-t-0 font-normal text-gray-500'>
+      <ProfileButton name={'Meows'} />
+      <ProfileButton name={'Replies'} />
+      <ProfileButton name={'Highlights'} />
+      <ProfileButton name={'Media'} />
+      <ProfileButton name={'Likes'} />
+    </div>
+  );
+};
+
+const ProfileButton = ({ name }: { name: string }) => {
+  return (
+    <div className='flex-center btnhover flex-1 !rounded-none px-2 lg:px-4'>
+      <div className='flex-center h-full'>
+        <span className='text-sm lg:text-base'>{name}</span>
+      </div>
+    </div>
+  );
+};
