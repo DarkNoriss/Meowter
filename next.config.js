ab9de115c7e15d87892613e9a799f28f6509f6de
Patryk Przybylski <darknoriss@gmail.com>
2023-07-13 08:22:57 +0200

    remove: no-cache for whole site

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
