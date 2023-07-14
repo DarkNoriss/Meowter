23b8308b23165c54980512f26fbffb2cd5e71d20
Patryk Przybylski <darknoriss@gmail.com>
2023-07-14 02:18:40 +0200

    styling

--- components/MeowCard/MeowCard.tsx
+++ components/MeowCard/MeowCard.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 import { MeowWithAuthor } from '@/types/custom-types';
 import { CardHeader } from './CardHeader';
 import { CardContext } from './CardContext';
@@ -8,7 +8,7 @@ import { CardReactions } from './CardReactions';
 export const MeowCard = ({ meow }: { meow: MeowWithAuthor }) => {
   return (
     <article className='border-white-smoll !border-t-0 px-4'>
-      <div className='flex max-w-full flex-row py-3'>
+      <div className='flex max-w-full flex-row p-3'>
         <div className='mr-3 min-w-max'>
           <Image
             src={meow.author.avatar}
