7b5bf7d13333dabea4f64eb456c2ec7adee40143
Patryk Przybylski <darknoriss@gmail.com>
2023-07-14 03:17:14 +0200

    fix: some svg problem

--- next.config.js
+++ next.config.js
@@ -6,8 +6,12 @@ const nextConfig = {
   serverRuntimeConfig: {
     externalServer: true,
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
+    // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
+    // into React components. See https://react-svgr.com/docs/next/
+
     // Grab the existing rule that handles SVG imports
+    // @ts-ignore - rules is a private property that is not typed
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
     config.module.rules.push(
@@ -20,8 +24,8 @@ const nextConfig = {
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ['@svgr/webpack'],
       }
     );
