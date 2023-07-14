0e1eca5b1a374c530b850ed7bec9eb4b4efcbe74
Patryk Przybylski <darknoriss@gmail.com>
2023-07-14 03:57:34 +0200

    cleaning, use svgr for arrow

--- app/(profile)/[id]/page.tsx
+++ app/(profile)/[id]/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import useSWR from 'swr';
+import ArrowIcon from '@/public/assets/icons/arrow.svg';
 import { useSession } from 'next-auth/react';
 import { UserWithMeows } from '@/types/custom-types';
 import { Feed } from '@/components/Feed';
@@ -19,20 +20,16 @@ const Profile = ({ params }: { params: { id: string } }) => {
       {data && (
         <div key={data.id}>
           <div className='border-white-smoll bg-transblur sticky top-0 z-10 flex h-14 w-full max-w-xl items-center !border-t-0 px-4 backdrop-blur-md backdrop-filter'>
-            <div className='btnhover mr-4 p-2'>
-              <Link href='/' className='min-w-[56px]' passHref>
-                <Image src={`/assets/icons/arrow.svg`} alt='logo' height={22} width={22} />
-              </Link>
-            </div>
+            <Link href='/' className='btnhover mr-4 p-2' passHref>
+              <ArrowIcon alt='Arrow' className='aspect-square h-6 fill-gray-100' />
+            </Link>
             <div className='flex flex-col'>
               <span className='text-xl font-bold'>{data.username}</span>
               <span className='text-sm text-gray-500'>{data.meows.length} Meows</span>
             </div>
           </div>
           <div className='border-white-smoll !border-t-0'>
-            <div>
-              <Image src={`/assets/img/bg.jpg`} alt='logo' height={500} width={1500} />
-            </div>
+            <Image src={`/assets/img/bg.jpg`} alt='Background image' height={500} width={1500} />
             <div className='mb-4 px-4 pt-3'>
               <div className='-mb-4 flex justify-between lg:-mb-6'>
                 <div className='mt-[15%] w-1/4'>
