06377f4ff008de0b8d50f8484f20d9c5ead8e805
Patryk Przybylski <darknoriss@gmail.com>
2023-07-15 10:51:12 +0200

    add: layout for profile, working on profile

--- components/Profile/ProfileNavigation.tsx
+++ components/Profile/ProfileNavigation.tsx
@@ -1,3 +1,5 @@
+import { usePathname } from 'next/navigation';
+
 export const ProfileNavigation = () => {
   return (
     <div className='border-white-smoll flex h-14 flex-row !border-t-0 font-normal text-gray-500'>
@@ -11,6 +13,9 @@ export const ProfileNavigation = () => {
 };
 
 const ProfileButton = ({ name }: { name: string }) => {
+  const path = usePathname();
+
+  console.log(path);
   return (
     <div className='flex-center btnhover flex-1 !rounded-none px-2 lg:px-4'>
       <div className='flex-center h-full'>
