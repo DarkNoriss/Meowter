52cee84bf86ab4a8fa195480ad4ec53568c9455e
Patryk Przybylski <darknoriss@gmail.com>
2023-07-16 17:03:09 +0200

    add: display what meows user liked

--- app/(profile)/[id]/likes/page.tsx
+++ app/(profile)/[id]/likes/page.tsx
@@ -1,9 +1,21 @@
+'use client';
+
+import useSWR from 'swr';
+import { UserWithMeows } from '@/types/custom-types';
+import { fetcher } from '@/utils/useSWRFetcher';
+import { MeowCard } from '@/components/MeowCard/MeowCard';
+
 const ProfileLikes = ({ params }: { params: { id: string } }) => {
-  return (
-    <div className='flex-center'>
-      <span>Likes</span>
-    </div>
-  );
+  const { data } = useSWR<UserWithMeows>(`/api/users/${params.id}/likes`, fetcher);
+
+  if (data)
+    return (
+      <>
+        {data.map((like: any) => (
+          <MeowCard key={like.id} meow={like.meow} />
+        ))}
+      </>
+    );
 };
 
 export default ProfileLikes;
