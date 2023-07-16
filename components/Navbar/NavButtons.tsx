4d093ab9c37f063555e10f047f49dbafa6262313
Patryk Przybylski <darknoriss@gmail.com>
2023-07-16 23:15:22 +0200

    cleaning the code

--- components/Navbar/NavButtons.tsx
+++ components/Navbar/NavButtons.tsx
@@ -3,61 +3,58 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { clsx } from 'clsx';
 
 export const NavButtons = () => {
+  const { data: session } = useSession();
+  const path = usePathname();
+  const navigationMap: { [key: string]: string } = {
+    home: `/`,
+    profile: `/${session?.user.link}`,
+  };
+
+  const buttons = [
+    { name: 'home', link: `/` },
+    { name: 'explore', link: `/` },
+    { name: 'notifications', link: `/` },
+    { name: 'messages', link: `/` },
+    { name: 'lists', link: `/` },
+    { name: 'bookmarks', link: `/` },
+    { name: 'communities', link: `/` },
+    { name: 'verified', link: `/` },
+    { name: 'profile', link: `/${session?.user.link}` },
+    { name: 'more', link: `/` },
+  ];
+
+  const isActive = (name: string) => {
+    if (name === 'profile') {
+      const regex = new RegExp(`^/${session?.user.link}`);
+      return regex.test(path);
+    }
+
+    return path === navigationMap[name];
+  };
+
   return (
     <div className='my-1'>
-      <Link href='/' passHref>
-        <Button name={'home'} />
-      </Link>
-      <Buttons />
+      {buttons.map((button) => (
+        <NavButton key={button.name} name={button.name} link={button.link} isActive={isActive(button.name)} />
+      ))}
     </div>
   );
 };
 
-const Buttons = () => {
-  const { data: session } = useSession();
-
-  if (session)
-    return (
-      <>
-        <Button name={'explore'} />
-        <Button name={'notifications'} />
-        <Button name={'messages'} />
-        <Button name={'lists'} />
-        <Button name={'booksmarks'} />
-        <Button name={'verified'} />
-        <Link href={`/${session.user.link}`} passHref>
-          <Button name={'profile'} link={session.user.link} />
-        </Link>
-        <Button name={'more'} />
-      </>
-    );
-};
-
-const Button = ({ name, link }: { name: string; link?: string }) => {
-  const [isHere, setIsHere] = useState<boolean>(false);
-  const path = usePathname();
-
-  console.log();
-  useEffect(() => {
-    const isPathMatched = (name === 'home' && path === '/') || path === `/${name}` || path === `/${link}`;
-
-    setIsHere(isPathMatched);
-  }, [link, name, path]);
-
+const NavButton = ({ name, link, isActive }: { name: string; link: string; isActive: boolean }) => {
   return (
-    <div className='flex h-14 justify-end xl:mr-6'>
+    <Link href={link} className='flex h-14 justify-end xl:mr-6'>
       <div className='btnhover flex-center my-1 h-14 w-14 flex-row p-3 xl:flex xl:w-full xl:items-start xl:justify-start'>
         <div className='flex-center h-7 w-7'>
           <Image src={`/assets/icons/${name}.svg`} alt='logo' height={28} width={28} className='h-full w-full' />
         </div>
-        <span className={clsx(`ml-5 hidden text-xl capitalize xl:block`, `${isHere ? 'font-bold' : ''}`)}>{name}</span>
+        <span className={clsx(`ml-5 hidden text-xl capitalize xl:block`, `${isActive ? 'font-bold' : ''}`)}>{name}</span>
       </div>
-    </div>
+    </Link>
   );
 };
