a774a264aaaf4c5c0b8a587a2a8286af73a768b1
Patryk Przybylski <darknoriss@gmail.com>
2023-07-16 17:10:50 +0200

    cleaning

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -6,13 +6,6 @@ export const Logo = () => {
     <div className='mt-2 flex justify-end xl:justify-start'>
       <Link href='/' className='btnhover flex-center aspect-square h-14 py-[2px]' passHref>
         <CatIcon alt='cat' className='flex-center ml-3 aspect-square h-7' />
-        {/* <Image
-          src='/assets/icons/cat.svg'
-          alt='logo'
-          height={28}
-          width={28}
-          className='h-8 w-auto'
-        /> */}
       </Link>
     </div>
   );
