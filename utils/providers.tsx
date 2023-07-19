491fb56fa414254016100f9f4658ac0941d96623
Patryk Przybylski <darknoriss@gmail.com>
2023-07-19 22:58:46 +0200

    move: queryClient inside component

--- utils/providers.tsx
+++ utils/providers.tsx
@@ -3,10 +3,11 @@
 import { SessionProviderType } from '@/types/custom-types';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
-
-const queryClient = new QueryClient();
+import { useState } from 'react';
 
 export const Providers = ({ children, session }: SessionProviderType) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
