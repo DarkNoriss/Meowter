66bca0747b96a76ca54b90aa1f83eedfb802792d
Patryk Przybylski <darknoriss@gmail.com>
2023-07-20 05:08:36 +0200

    add: api to send replies

---
+++ app/api/reply/new/route.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from 'next-auth';
+import { options } from '../../auth/[...nextauth]/options';
+import { prisma } from '@/utils/connectToDb';
+
+export const POST = async (req: Request) => {
+  const { meowId, context } = await req.json();
+  const session = await getServerSession(options);
+
+  try {
+    if (session) {
+      const newReply = await prisma.reply.create({
+        data: {
+          userId: session.user.id as string,
+          meowId: meowId,
+          text: context,
+        },
+      });
+
+      return new Response(JSON.stringify(newReply), { status: 201 });
+    }
+  } catch (err) {
+    return new Response('Failed to create a new reply', { status: 500 });
+  }
+};
