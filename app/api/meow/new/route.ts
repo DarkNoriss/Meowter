ea235cafe51cba83d590b1349b78e3bae197230a
Patryk Przybylski <darknoriss@gmail.com>
2023-07-23 11:39:04 +0200

    add: type for api req

--- app/api/meow/new/route.ts
+++ app/api/meow/new/route.ts
@@ -1,23 +1,27 @@
-import { getServerSession } from 'next-auth';
-import { options } from '../../auth/[...nextauth]/options';
-import { prisma } from '@/utils/connectToDb';
+import { getServerSession } from "next-auth"
+import { options } from "../../auth/[...nextauth]/options"
+import { prisma } from "@/utils/connectToDb"
+
+type ResponseData = {
+  context: string
+}
 
 export const POST = async (req: Request) => {
-  const { context } = await req.json();
-  const session = await getServerSession(options);
+  const session = await getServerSession(options)
+  const { context } = (await req.json()) as ResponseData
 
   try {
     if (session) {
       const newMeow = await prisma.meow.create({
         data: {
-          userId: session.user.id as string,
+          userId: session.user.id,
           text: context,
         },
-      });
+      })
 
-      return new Response(JSON.stringify(newMeow), { status: 201 });
+      return new Response(JSON.stringify(newMeow), { status: 201 })
     }
   } catch (err) {
-    return new Response('Failed to create a new meow', { status: 500 });
+    return new Response("Failed to create a new meow", { status: 500 })
   }
-};
+}
--- app/api/reply/new/route.ts
+++ app/api/reply/new/route.ts
@@ -1,10 +1,11 @@
-import { getServerSession } from 'next-auth';
-import { options } from '../../auth/[...nextauth]/options';
-import { prisma } from '@/utils/connectToDb';
+import { getServerSession } from "next-auth"
+import { options } from "../../auth/[...nextauth]/options"
+import { prisma } from "@/utils/connectToDb"
+import { Reply } from "@prisma/client"
 
 export const POST = async (req: Request) => {
-  const { meowId, text } = await req.json();
-  const session = await getServerSession(options);
+  const session = await getServerSession(options)
+  const { meowId, text } = (await req.json()) as Reply
 
   try {
     if (session) {
@@ -14,11 +15,11 @@ export const POST = async (req: Request) => {
           meowId,
           text,
         },
-      });
+      })
 
-      return new Response(JSON.stringify(newReply), { status: 201 });
+      return new Response(JSON.stringify(newReply), { status: 201 })
     }
   } catch (err) {
-    return new Response('Failed to create a new reply', { status: 500 });
+    return new Response("Failed to create a new reply", { status: 500 })
   }
-};
+}
