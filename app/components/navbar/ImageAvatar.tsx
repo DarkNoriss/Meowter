92b936ee885bb6946c12e18b7e4e6dd7140ef092
Patryk Przybylski <darknoriss@gmail.com>
2023-07-29 22:50:50 +0200

    moving components and lib

--- app/components/navbar/ImageAvatar.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image"
-import { useSession } from "next-auth/react"
-
-export const ImageAvatar = () => {
-  const { data: session } = useSession()
-
-  return <Image src={session?.user?.image as string} alt="Avatar" height={40} width={40} className="rounded-full" />
-}
--- app/lib/strToLink.ts
+++
@@ -1,3 +0,0 @@
-export const strToLink = (str: string) => {
-  return str.replace(/\s/g, '').toLowerCase();
-};
